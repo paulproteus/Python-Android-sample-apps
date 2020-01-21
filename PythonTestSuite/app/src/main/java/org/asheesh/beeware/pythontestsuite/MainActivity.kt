9754a6090a8545a644fcde036ce238b00295dead
Asheesh Laroia <asheesh@asheesh.org>
2020-01-21 12:07:34 -0800

    Stop loading libraries unnecessarily

--- PythonTestSuite/app/src/main/java/org/asheesh/beeware/pythontestsuite/MainActivity.kt
+++ PythonTestSuite/app/src/main/java/org/asheesh/beeware/pythontestsuite/MainActivity.kt
@@ -139,8 +139,6 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
-            System.loadLibrary("python3.7m")
-            System.loadLibrary("rubicon")
         }
     }
 }
