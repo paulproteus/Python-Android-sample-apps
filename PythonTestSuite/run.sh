2aed86a719b16802733ad3be67e2c234c726c9c2
Asheesh Laroia <asheesh@asheesh.org>
2020-01-22 13:37:07 -0800

    Add a script to launch the test app

---
+++ PythonTestSuite/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Strict mode for bash.
+set -eou pipefail
+
+APP="org.asheesh.beeware.pythontestsuite"
+MAIN_ACTIVITY="org.asheesh.beeware.pythontestsuite.MainActivity"
+
+# Build and install the app on the most easily accessible Android device.
+./gradlew installDebug
+
+# Clear and then watch the log.
+adb shell logcat -c
+adb shell logcat &
+
+# Stop the app, then launch it.
+adb shell am force-stop "$APP"
+USER_ID="$(adb shell dumpsys package org.asheesh.beeware.pythontestsuite | grep userId | sed s,userId=,,)"
+adb shell am start "org.asheesh.beeware.pythontestsuite/org.asheesh.beeware.pythontestsuite.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+
+# Foreground the logcat task.
+fg
