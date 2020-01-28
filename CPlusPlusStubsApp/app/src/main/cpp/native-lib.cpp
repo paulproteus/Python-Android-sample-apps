9c785499fec2b4b6cc542a62dd26ef48348e6be3
Asheesh Laroia <asheesh@asheesh.org>
2020-01-28 11:10:39 -0800

    Add activity lifecycle stubs

--- CPlusPlusStubsApp/app/src/main/cpp/native-lib.cpp
+++ CPlusPlusStubsApp/app/src/main/cpp/native-lib.cpp
@@ -1,4 +1,5 @@
 #include <jni.h>
+#include "android/log.h"
 #include <string>
 
 extern "C" JNIEXPORT jobject JNICALL
@@ -6,7 +7,8 @@ Java_org_asheesh_beeware_cplusplusstubsapp_MainActivity_createButton(
         JNIEnv *env,
         jobject thisObj) {
     jclass buttonClass = env->FindClass("android/widget/Button");
-    jmethodID buttonConstructor = env->GetMethodID(buttonClass, "<init>", "(Landroid/content/Context;)V");
+    jmethodID buttonConstructor = env->GetMethodID(buttonClass, "<init>",
+                                                   "(Landroid/content/Context;)V");
     jobject button = env->NewObject(buttonClass, buttonConstructor, thisObj);
     return button;
 //    return env->NewStringUTF(hello.c_str());
@@ -18,4 +20,12 @@ Java_org_asheesh_beeware_cplusplusstubsapp_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_org_asheesh_beeware_cplusplusstubsapp_MainActivity_nativeLog(JNIEnv *env,
+                                                                  jobject /* this */,
+                                                                  jstring message) {
+    const char *messageUTF = env->GetStringUTFChars(message, NULL);
+    __android_log_write(ANDROID_LOG_DEBUG, "cplusplusstubsapp.nativeLog", messageUTF);
 }
--- CPlusPlusStubsApp/app/src/main/java/org/asheesh/beeware/cplusplusstubsapp/MainActivity.java
+++ CPlusPlusStubsApp/app/src/main/java/org/asheesh/beeware/cplusplusstubsapp/MainActivity.java
@@ -1,12 +1,11 @@
 package org.asheesh.beeware.cplusplusstubsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        nativeLog("onCreate called");
+
         LinearLayout layout = new LinearLayout(this);
         setContentView(layout);
         createButton();
@@ -27,16 +28,37 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ((Button) v).setText(stringFromJNI());
+                ((Button) v).setText("Renamed the button from Java");
             }
         });
         layout.addView(button);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        nativeLog("onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nativeLog("onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nativeLog("onPause called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        nativeLog("onDestroy called");
+    }
+
     private native Button createButton();
+
+    private native void nativeLog(String msg);
 }
