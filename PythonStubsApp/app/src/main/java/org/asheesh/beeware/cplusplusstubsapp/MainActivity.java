40841b827cc285db14a7f3387e2c69e05499e7f8
Asheesh Laroia <asheesh@asheesh.org>
2020-01-30 12:00:47 -0800

    Copy CPlusPlusStubsApp into Python app

---
+++ PythonStubsApp/app/src/main/java/org/asheesh/beeware/cplusplusstubsapp/MainActivity.java
@@ -0,0 +1,42 @@
+package org.asheesh.beeware.cplusplusstubsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        setContentView(layout);
+        createButton();
+        Button button = createButton();
+        // TODO: Move the next two expressions into C++.
+        button.setText("Button created from Java & C++ together");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Button) v).setText(stringFromJNI());
+            }
+        });
+        layout.addView(button);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    private native Button createButton();
+}
