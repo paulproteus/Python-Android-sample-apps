aa15f789acaa0cd70d4b19d81991208ec4e10d2e
Asheesh Laroia <asheesh@asheesh.org>
2020-02-03 23:10:00 -0800

    Add cmake path to $PATH

--- PythonTestSuite/run.sh
+++ PythonTestSuite/run.sh
@@ -8,8 +8,9 @@ MAIN_ACTIVITY="org.asheesh.beeware.pythontestsuite.MainActivity"
 # Tell bash to kill the logcat when this script exits.
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-# Build and install the app on the most easily accessible Android device.
-./gradlew installDebug
+# Build and install the app on the most easily accessible Android device,
+# with the cmake directory specifically on the PATH.
+PATH="$PATH:${ANDROID_SDK_ROOT}/cmake/3.10.2.4988404/bin" ./gradlew installDebug
 
 # Clear and then watch the log.
 adb shell logcat -c
