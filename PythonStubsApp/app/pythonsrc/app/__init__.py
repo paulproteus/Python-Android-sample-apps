933d774bab03931f0ad697db9a7a5cc070bd8722
Asheesh Laroia <asheesh@asheesh.org>
2020-02-10 12:50:12 -0800

    Add network permissions & TLS cert mangling

--- PythonStubsApp/app/pythonsrc/app/__init__.py
+++ PythonStubsApp/app/pythonsrc/app/__init__.py
@@ -1,7 +1,11 @@
 import datetime
+import glob
 import json
+import os
 import random
 import urllib.request
+import socket
+import ssl
 
 
 def print_int_list():
@@ -9,8 +13,8 @@ def print_int_list():
     print("random integers from 0 to 99 (inclusive)", l)
 
 
-def print_beeware_members():
-    response = urllib.request.urlopen("https://api.github.com/orgs/beeware/members")
+def print_beeware_members(context):
+    response = urllib.request.urlopen("https://api.github.com/orgs/beeware/members", context=context)
     body = response.read()
     parsed = json.loads(body)
     print([item["login"] for item in parsed])
@@ -22,11 +26,31 @@ def print_now():
     utcnow = datetime.datetime.utcnow()
     print("Current time in UTC", utcnow.isoformat())
 
+def make_ssl_context():
+    if os.path.exists('/etc/security/cacerts'):
+        # Running on Android
+        print("Using Android-specific SSL context")
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        bundle_contents = ''
+        for filename in glob.glob('/etc/security/cacerts/*'):
+            with open(filename) as fd:
+                s = fd.read()
+                if 'END CERTIFICATE' in s:
+                    lines = s.split('\n')
+                    line_end_certificate = [i for i, line in enumerate(lines) if 'END CERTIFICATE' in line][0]
+                    bundle_contents += '\n'.join(lines[0:line_end_certificate+1]) + '\n'
+        bundle_path = os.environ['TMPDIR'] + '/bundle'
+        with open(bundle_path, 'w') as fd:
+            fd.write(bundle_contents)
+        context.load_verify_locations(bundle_path)
+        return context
+    return ssl.create_default_context()
 
 def do_everything():
+    context = make_ssl_context()
     print_now()
     print_int_list()
-    print_beeware_members()
+    print_beeware_members(context)
 
 
 class Application:
